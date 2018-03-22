d2b212c4fad496e632ca1a74c5a9b562d78f50cd
Lei (Leo) Zhao <Leo@zhaodeveloper.com>
2018-03-22 12:04:48 +1100

    transfer data to single file

---
+++ data/customerdata.js
@@ -0,0 +1,63 @@
+var dataVm =
+    [
+        {
+            "Id": 0,
+            "isActive": true,
+            "Name": "Helga",
+            "Address": "700 Columbus Place, Durham, Georgia, 2063"
+        },
+        {
+            "Id": 1,
+            "isActive": true,
+            "Name": "Leblanc",
+            "Address": "402 Huntington Street, Darlington, Virgin Islands, 4714"
+        },
+        {
+            "Id": 2,
+            "isActive": true,
+            "Name": "Donna",
+            "Address": "167 Noel Avenue, Santel, New Mexico, 2424"
+        },
+        {
+            "Id": 3,
+            "isActive": true,
+            "Name": "Wright",
+            "Address": "598 Flatlands Avenue, Nescatunga, Guam, 5751"
+        },
+        {
+            "Id": 4,
+            "isActive": true,
+            "Name": "Pansy",
+            "Address": "889 Freeman Street, Westphalia, Delaware, 2721"
+        },
+        {
+            "Id": 5,
+            "isActive": true,
+            "Name": "Penny",
+            "Address": "822 Tabor Court, Blanford, Michigan, 2230"
+        },
+        {
+            "Id": 6,
+            "isActive": true,
+            "Name": "Wilkins",
+            "Address": "900 Tennis Court, Matthews, Maryland, 1835"
+        },
+        {
+            "Id": 7,
+            "isActive": true,
+            "Name": "Margret",
+            "Address": "800 Beaver Street, Lopezo, Alaska, 8243"
+        },
+        {
+            "Id": 8,
+            "isActive": true,
+            "Name": "Nancy",
+            "Address": "341 Lester Court, Abrams, Pennsylvania, 2512"
+        },
+        {
+            "Id": 9,
+            "isActive": true,
+            "Name": "Debbie",
+            "Address": "953 Hanson Place, Manila, Marshall Islands, 1272"
+        }
+    ]
