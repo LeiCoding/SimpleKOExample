6d4df8ed9cd8f9bfb26ac85c5ee997852f5c07c3
Lei (Leo) Zhao <Leo@zhaodeveloper.com>
2018-03-29 10:35:18 +1100

    separate parts
    separate data from page and js file

---
+++ scripts/customerdata.js
@@ -0,0 +1,27 @@
+var dataVm =
+    [
+        {
+            "Id": 0,
+            "isActive": true,
+            "Name": "Helga",
+            "Address": "700 Columbus Place, Durham, Georgia, 2063"
+        },
+        {
+            "Id": 1,
+            "isActive": true,
+            "Name": "Leblanc",
+            "Address": "402 Huntington Street, Darlington, Virgin Islands, 4714"
+        },
+        {
+            "Id": 2,
+            "isActive": true,
+            "Name": "Donna",
+            "Address": "167 Noel Avenue, Santel, New Mexico, 2424"
+        },
+        {
+            "Id": 3,
+            "isActive": true,
+            "Name": "Wright",
+            "Address": "598 Flatlands Avenue, Nescatunga, Guam, 5751"
+        }
+    ]
